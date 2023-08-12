67e5516b03c0525d22177d029416d5dd942ca58f
facufer123 <fernandezfacundo10@gmail.com>
2023-08-11 21:14:39 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded",() =>{
 const div=document.getElementById("contenedor");
-div.addEventListener("click",()=>{
+const bot=document.getElementById("boton");
+bot.addEventListener("click", (e)=>{
+    alert("Hola");
+    e.stopPropagation();
+})
+div.addEventListener("click",(e)=>{
     alert("Hola! Soy el div");
 })
 })
